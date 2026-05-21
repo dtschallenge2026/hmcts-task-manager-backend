8b25b4fa138dd575eab8a59e02f57d24d25bbf98
DTS Applicant <286347526+dtschallenge2026@users.noreply.github.com>
2026-05-22 00:01:08 +0100

    Add task CRUD endpoints and PostgreSQL database setup

---
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.reform.dev.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import uk.gov.hmcts.reform.dev.models.Task;
+import uk.gov.hmcts.reform.dev.models.TaskStatus;
+import uk.gov.hmcts.reform.dev.services.TaskService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PatchMapping("/{id}/status")
+    public Task updateTaskStatus(@PathVariable Long id, @RequestBody TaskStatus status) {
+        return taskService.updateTaskStatus(id, status);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/dev/services/TaskService.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.dev.services;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.dev.models.Task;
+import uk.gov.hmcts.reform.dev.repositories.TaskRepository;
+import uk.gov.hmcts.reform.dev.models.TaskStatus;
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public Task updateTaskStatus(Long id, TaskStatus status) {
+        Task task = getTaskById(id);
+        task.setStatus(status);
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
