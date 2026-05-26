5b0a0461a5900772291fc0b69d5a9c436c6b068c
DTS Applicant <286347526+dtschallenge2026@users.noreply.github.com>
2026-05-26 21:37:27 +0100

    Order tasks by ID ascending in getAllTasks - Service change

--- src/main/java/uk/gov/hmcts/reform/dev/services/TaskService.java
+++ src/main/java/uk/gov/hmcts/reform/dev/services/TaskService.java
@@ -27,7 +27,7 @@ public class TaskService {
     }
 
     public List<Task> getAllTasks() {
-        return taskRepository.findAllByDeletedAtIsNull();
+        return taskRepository.findAllByDeletedAtIsNullOrderByIdAsc();
     }
 
     public Task getTaskById(Long id) {
